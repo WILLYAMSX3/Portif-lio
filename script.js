923d64ade0d87a2f7bd55e348e660bc6d257034d
Willyamsx333 <willsenai333@gmail.com>
2024-11-26 23:08:41 -0300

    adicionando Logo no  Projeto e começando fazer o javascript

---
+++ script.js
@@ -0,0 +1,14 @@
+/* function ProjetosNome(){
+   const js_projetosAnimais = document.querySelectorAll('.js_projetosImages a'); 
+    const js_tituloProjestos = document.querySelector('.tituloimg');
+
+   
+    js_projetosAnimais.addEventListener('click', tituloProjetos){
+        function tituloProjetos(){
+            if(js_tituloProjestos.getAttribute('class') == '.tituloimg'){
+                    js_tituloProjestos.classList.remove('tituloimg');
+                    js_projetosAnimais.classList.
+            }
+        }
+    }
+} */
