b8215a90c82887b4a99f355777e6d107493b3a9c
Willyamsx333 <willsenai333@gmail.com>
2025-11-06 21:27:01 -0300

    Mensagem do commit

--- script.js
+++ script.js
@@ -1,14 +1,8 @@
-/* function ProjetosNome(){
-   const js_projetosAnimais = document.querySelectorAll('.js_projetosImages a'); 
-    const js_tituloProjestos = document.querySelector('.tituloimg');
+const tituloDev = document.querySelector(".titulo");
 
-   
-    js_projetosAnimais.addEventListener('click', tituloProjetos){
-        function tituloProjetos(){
-            if(js_tituloProjestos.getAttribute('class') == '.tituloimg'){
-                    js_tituloProjestos.classList.remove('tituloimg');
-                    js_projetosAnimais.classList.
-            }
-        }
-    }
-} */
+tituloDev.addEventListener("mouseover", ()=>{
+    tituloDev.style.color = "red";
+    tituloDev.addEventListener("click", ()=>{
+        tituloDev.style.color = "green"
+    })
+})
